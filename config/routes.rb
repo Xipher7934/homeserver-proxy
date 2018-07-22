98c06765f817abd665506d8953d8c90779712cae
Ethan Heming <heming.e@gmail.com>
2018-07-22 19:12:39 -0400

    Initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "application#index"
+  root to: 'application#index'
 
-  post 'netflix', to: 'application#netflix'
-
-  post 'netflixcontrol', to: 'application#netflixcontrol'
-
-  post 'bootup', to: 'application#bootup'
-
-  post 'closeprogram', to: 'application#closeprogram'
-
-  post 'shutdown', to: 'application#shutdown'
-
-  post 'restart', to: 'application#restart'
+  match '*path' => 'application#index', via: :all
 end
