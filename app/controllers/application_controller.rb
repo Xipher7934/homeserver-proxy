c66b21cca6a9e954042eb0ac8168c1fa7a061eb4
Ethan Heming <heming.e@gmail.com>
2018-07-22 18:51:49 -0400

    Initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ReverseProxy::Controller
+
+  def index
+    reverse_proxy "http://107.190.8.176:4000", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
 end
+
