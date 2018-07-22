e1ecba7bd805a0b3554daf3f6acca6b3d1aee2d3
Ethan Heming <heming.e@gmail.com>
2018-07-22 19:07:18 -0400

    Initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < ActionController::API
       end
     end
   end
+
+  def bootup
+    reverse_proxy "http://107.190.8.176:4000/bootup", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
+
+  def netflix
+    reverse_proxy "http://107.190.8.176:4000/netflix", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
+
+  def closeprogram
+    reverse_proxy "http://107.190.8.176:4000/closeprogram", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
+
+  def shutdown
+    reverse_proxy "http://107.190.8.176:4000/shutdown", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
+
+  def restart
+    reverse_proxy "http://107.190.8.176:4000/restart", verify_ssl: false do |config|
+      # We got a 404!
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "application#index"
+
+  post 'netflix', to: 'application#netflix'
+
+  post 'netflixcontrol', to: 'application#netflixcontrol'
+
+  post 'bootup', to: 'application#bootup'
+
+  post 'closeprogram', to: 'application#closeprogram'
+
+  post 'shutdown', to: 'application#shutdown'
+
+  post 'restart', to: 'application#restart'
 end
